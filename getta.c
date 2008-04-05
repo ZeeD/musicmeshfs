9eabd01f9540b5fb01bc8fe57ae28c3d9ab493ed
Vito.DeTullio <Vito.DeTullio@f261d66c-0a4a-0410-b504-d369bbc55f03>
2008-04-05 19:00:51 +0000

    Versione iniziale di getta

---
+++ getta.c
@@ -0,0 +1,27 @@
+#include <stdio.h>   /* fprintf(), stderr, printf() */
+#include <tag_c.h>   /* taglib_file_new(), taglib_file_tag(), taglib_tag_year(),
+        taglib_tag_comment(), taglib_tag_free_strings(), taglib_tag_track(),
+        taglib_tag_genre(), taglib_file_free(), taglib_tag_title(),
+        taglib_tag_album(), taglib_tag_artist() */
+
+// cc -I/usr/include/taglib -ltag_c -o getta getta.c -W -Wall -std=c99 -pedantic
+
+int main(int argc, char* argv[]) {
+    for (int i=1; i<argc; i++) {
+        TagLib_File* tlf = taglib_file_new(argv[i]);
+        if (!tlf) {
+            fprintf(stderr, "WARN: `%s' non è supportato\n", argv[i]);
+            continue;
+        }
+        TagLib_Tag* tlt = taglib_file_tag(tlf);
+        printf("---[`%s']---\n" "artista  = `%s'\n" "anno     = %d\n"
+                "album    = `%s'\n" "traccia  = %d\n" "titolo   = `%s'\n"
+                "genere   = `%s'\n" "commento = `%s'\n" "\n", argv[i],
+                taglib_tag_artist(tlt), taglib_tag_year(tlt),
+                taglib_tag_album(tlt), taglib_tag_track(tlt),
+                taglib_tag_title(tlt), taglib_tag_genre(tlt),
+                taglib_tag_comment(tlt));
+        taglib_tag_free_strings();
+        taglib_file_free(tlf);
+    }
+}
