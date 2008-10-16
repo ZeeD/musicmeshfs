c0646b3d22f73072a085d5559986acb984c7e212
Vito.DeTullio <Vito.DeTullio@f261d66c-0a4a-0410-b504-d369bbc55f03>
2008-10-16 17:21:48 +0000

    yawn

--- trunk/stfs/mkfs.hixosfs.py
+++ trunk/stfs/mkfs.hixosfs.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
+# -*- encoding: utf-8 -*-
 
-from sys import stdout
+BLOCK_SIZE = 512
 
-def fill(string, size, filler='\0'):
+def fill(string, size=BLOCK_SIZE, filler='\0'):
     return string[:size] + ''.join(filler for _ in xrange(len(string), size))
 
-stdout.write(fill('HiXoS_FS', 512))
-
-stdout.write(fill(fill('pippo', 16), 512, 'R'))
-stdout.write(fill(fill('pluto', 16), 512, 'S'))
-stdout.write(fill(fill('topolino', 16), 512, 'T'))
+if __name__ == '__main__':
+    from sys import argv
+    if len(argv) != 3:
+        # FIXME: trovare un modo per capire da solo la dimensione, in caso di device
+        raise SystemExit("Uso: %s device size\n" % argv[0])
+    out = file(argv[1], 'wb')
+    out.write(fill('HiXoS_FS'))     # il primo blocco è già stato scritto
+    for _ in xrange(BLOCK_SIZE, int(argv[2]), BLOCK_SIZE):
+        out.write('\0'*BLOCK_SIZE)
+    out.close()
